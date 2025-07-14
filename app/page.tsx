183e272684a64ef97b4c332b4e6e7ee9ed1d3291
stanislaw.grin <stanislaw.grin@gmail.com>
2025-07-14 20:01:41 +0300

    Add companion card

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import { CompanionCard } from '@/components/companion-card'
+import { CompanionsList } from '@/components/companions-list'
+import { CTA } from '@/components/cta'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Lets Start</h1>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="Science"
+          duration={42}
+          color="#E5D0FF"
+        />
+        <CompanionCard
+          id="234"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="Maths"
+          duration={42}
+          color="#FFDA6E"
+        />
+        <CompanionCard
+          id="345"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="Language"
+          duration={42}
+          color="#BDE7FF"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
---
+++ components/nav-items.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { cn } from '@/lib/utils'
+
+const navItems = [
+  { label: 'Home', href: '/' },
+  { label: 'Companions', href: '/companions' },
+  { label: 'My Journey', href: '/my-journey' },
+]
+
+export const NavItems = () => {
+  const pathname = usePathname()
+
+  return (
+    <nav className="flex items-center gap-4">
+      {navItems.map(({ label, href }) => (
+        <Link href={href} key={href} className={cn(pathname === href && 'text-primary font-semibold')}>
+          {label}
+        </Link>
+      ))}
+    </nav>
+  )
+}
