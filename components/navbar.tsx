584d6a1d4f8f6617f4346578b9f8958eeb27c5c6
stanislaw.grin <stanislaw.grin@gmail.com>
2025-07-15 16:00:23 +0300

    Setup Auth & Billing

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,4 @@
+import { SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -8,13 +9,26 @@ export const Navbar = () => {
     <nav className="navbar">
       <Link href="/">
         <div className="flex items-center gap-2.5 cursor-pointer">
-          <Image src="/images/logo.svg" width={46} height={44} alt="Logo" />
+          <Image
+            src="/images/logo.svg"
+            width={46}
+            height={44}
+            alt="Logo"
+          />
         </div>
       </Link>
 
       <div className="flex items-center gap-8">
         <NavItems />
-        <p>Sign In</p>
+        <SignedOut>
+          <div className="flex items-center gap-4">
+            <SignInButton />
+            <SignUpButton />
+          </div>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   )
